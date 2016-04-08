4d4211c8820308229085f2c3a48792a3ba9160e2
Christophe Paccolat <christophe.paccolat@alumni.epfl.ch>
2016-04-08 23:04:59 +0200

    Fix Uppercase filename typo

--- _IOPlugins/ImportTimeseries/importTimeseries.cpp
+++ _IOPlugins/ImportTimeseries/importTimeseries.cpp
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free
 Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 *********************************************************************/
-#include "fileUtils.h"
+#include "fileutils.h"
 #include "importTimeseries.h"
 
 Q_EXPORT_PLUGIN2(IO_ImportTimeseries, ImportTimeseries)
